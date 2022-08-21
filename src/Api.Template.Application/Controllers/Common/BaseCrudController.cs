06d9aca5aa945780c13332370f40ce159b2bbf8f
tuanl1281 <tuanle.it.1280@gmail.com>
2022-08-21 22:14:17 +0700

    chore(@global): refactor base controller

--- src/Api.Template.Application/Controllers/Common/BaseCrudController.cs
+++ src/Api.Template.Application/Controllers/Common/BaseCrudController.cs
@@ -1,103 +1,60 @@
-﻿using AutoMapper;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Api.Template.Data.Infrastructures;
-using Api.Template.ViewModel.Common.Exception;
+using Api.Template.Service.Core.Common;
 using Api.Template.ViewModel.Common.Response;
 
-namespace Api.Template.Service.Core.Common;
+namespace Api.Template.Application.Controllers.Common;
 
 /*
-    Generic
-    - TE: Entity
-    - TF: Filter model
-    - TV: View model
-    - TA: Add model
-    - TU: Update model
+	Generic
+	- E: Entity
+	- F: Filter model
+	- V: View model
+	- A: Add model
+	- U: Update model
 */
 
-public interface IBaseService<TContext, TE, TF, TV, TA, TU> where TContext : DbContext where TE : class where TF : class where TV : class where TA : class where TU : class
+public class BaseController<TContext, TE, TF, TV, TA, TU> : BaseController where TContext : DbContext where TE : class where TF : class where TV : class where TA : class where TU : class
 {
-    Task<object> Add(TA model);
+    private readonly IBaseService<TContext, TE, TF, TV, TA, TU> _service;
 
-    Task<object> Update(TU model, Guid id);
-
-    Task<object> Delete(Guid id);
-
-    Task<TV> Get(Guid id);
-
-    Task<PagingResponseModel<TV>> GetPagedResult(TF filter, Guid userId);
-}
-
-public class BaseService<TContext, TE, TF, TV, TA, TU>: IBaseService<TContext, TE, TF, TV, TA, TU> where TContext : DbContext, new() where TE : class where TF : class where TV : class where TA : class where TU : class
-{
-    protected readonly IMapper _mapper;
-    protected readonly IUnitOfWork<TContext> _unitOfWork;
-
-    protected BaseService(IMapper mapper, IUnitOfWork<TContext> unitOfWork)
+    public BaseController(IBaseService<TContext, TE, TF, TV, TA, TU> service)
     {
-        _mapper = mapper;
-        _unitOfWork = unitOfWork;
+        _service = service;
     }
 
-    public virtual async Task<object> Add(TA model)
+    [HttpGet]
+    public async Task<ActionResult<PagingResponseModel>> GetPagedResult([FromQuery] TF filter)
     {
-        /* Builder */
-        var entity = _mapper.Map<TA, TE>(model);
-        /* Save */
-        _unitOfWork.Repository<TE>().Add(entity);
-        await _unitOfWork.SaveChangesAsync();
-        
-        return null;
+        var result = await _service.GetPagedResult(filter, Principal.UserId);
+        return BuildPagingResponse(result.Data, result.TotalCounts);
     }
 
-    public virtual async Task<object> Update(TU model, Guid id)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ResultResponseModel>> Get([FromRoute] Guid id)
     {
-        /* Validate */
-        var entity = await _unitOfWork.Repository<TE>().GetByIdAsync(id);
-        if (entity == null)
-            throw new NotFoundException();
-        /* Builder */
-        entity = _mapper.Map<TU, TE>(model, entity);
-        /* Save */
-        _unitOfWork.Repository<TE>().Update(entity);
-        await _unitOfWork.SaveChangesAsync();
-
-        return id;
+        var result = await _service.Get(id);
+        return BuildResultResponse(result);
     }
 
-    public virtual async Task<object> Delete(Guid id)
+    [HttpPost]
+    public async Task<ActionResult<ResultResponseModel>> Add([FromBody] TA model)
     {
-        /* Validate */
-        var entity = await _unitOfWork.Repository<TE>().GetByIdAsync(id);
-        if (entity == null)
-            throw new NotFoundException();
-        /* Save */
-        _unitOfWork.Repository<TE>().Delete(id);
-        await _unitOfWork.SaveChangesAsync();
-
-        return id;
+        var result = await _service.Add(model);
+        return BuildResultResponse(result);
     }
 
-    public virtual async Task<TV> Get(Guid id)
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ResultResponseModel>> Update([FromBody] TU model, [FromRoute] Guid id)
     {
-        /* Validate */
-        var entity = await _unitOfWork.Repository<TE>().GetByIdAsync(id);
-        if (entity == null)
-            throw new NotFoundException();
-        
-        return _mapper.Map<TE, TV>(entity);
+        var result = await _service.Update(model, id);
+        return BuildResultResponse(result);
     }
 
-    public virtual async Task<PagingResponseModel<TV>> GetPagedResult(TF filter, Guid userId)
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ResultResponseModel>> Delete([FromRoute] Guid id)
     {
-        var result = new PagingResponseModel<TV>();
-        /* Query */
-        var entities = await _unitOfWork.Repository<TE>().GetAllAsync();
-        /* Builder */
-        var entityModels = _mapper.Map<List<TE>, List<TV>>(entities);
-
-        result.TotalCounts = entities.Count;
-        result.Data = entityModels;
-        return result;
+        var result = await _service.Delete(id);
+        return BuildResultResponse(result);
     }
 }
--- src/Api.Template.Service/Core/Common/BaseService.cs
+++ src/Api.Template.Service/Core/Common/BaseService.cs
@@ -1,7 +1,6 @@
 using AutoMapper;
-using Api.Template.Data.Context;
+using Microsoft.EntityFrameworkCore;
 using Api.Template.Data.Infrastructures;
-using Api.Template.Data.Repositories.Common;
 using Api.Template.ViewModel.Common.Exception;
 using Api.Template.ViewModel.Common.Response;
 
@@ -16,7 +15,7 @@ namespace Api.Template.Service.Core.Common;
     - TU: Update model
 */
 
-public interface IBaseService<TE, TF, TV, TA, TU> where TE : class where TF : class where TV : class where TA : class where TU : class
+public interface IBaseService<TContext, TE, TF, TV, TA, TU> where TContext : DbContext where TE : class where TF : class where TV : class where TA : class where TU : class
 {
     Task<object> Add(TA model);
 
@@ -29,17 +28,15 @@ public interface IBaseService<TE, TF, TV, TA, TU> where TE : class where TF : cl
     Task<PagingResponseModel<TV>> GetPagedResult(TF filter, Guid userId);
 }
 
-public class BaseService<TE, TF, TV, TA, TU>: IBaseService<TE, TF, TV, TA, TU> where TE : class where TF : class where TV : class where TA : class where TU : class
+public class BaseService<TContext, TE, TF, TV, TA, TU>: IBaseService<TContext, TE, TF, TV, TA, TU> where TContext : DbContext, new() where TE : class where TF : class where TV : class where TA : class where TU : class
 {
     protected readonly IMapper _mapper;
-    protected readonly IUnitOfWork<SqlDbContext> _unitOfWork;
-    protected readonly IBaseRepository<TE, SqlDbContext> _repository;
+    protected readonly IUnitOfWork<TContext> _unitOfWork;
 
-    protected BaseService(IMapper mapper, IUnitOfWork<SqlDbContext> unitOfWork, IBaseRepository<TE, SqlDbContext> repository)
+    protected BaseService(IMapper mapper, IUnitOfWork<TContext> unitOfWork)
     {
         _mapper = mapper;
         _unitOfWork = unitOfWork;
-        _repository = repository;
     }
 
     public virtual async Task<object> Add(TA model)
@@ -47,8 +44,8 @@ public class BaseService<TE, TF, TV, TA, TU>: IBaseService<TE, TF, TV, TA, TU> w
         /* Builder */
         var entity = _mapper.Map<TA, TE>(model);
         /* Save */
-        _repository.Add(entity);
-        await _repository.DbContext.SaveChangesAsync();
+        _unitOfWork.Repository<TE>().Add(entity);
+        await _unitOfWork.SaveChangesAsync();
         
         return null;
     }
@@ -56,14 +53,14 @@ public class BaseService<TE, TF, TV, TA, TU>: IBaseService<TE, TF, TV, TA, TU> w
     public virtual async Task<object> Update(TU model, Guid id)
     {
         /* Validate */
-        var entity = await _repository.GetByIdAsync(id);
+        var entity = await _unitOfWork.Repository<TE>().GetByIdAsync(id);
         if (entity == null)
             throw new NotFoundException();
         /* Builder */
         entity = _mapper.Map<TU, TE>(model, entity);
         /* Save */
-        _repository.Update(entity);
-        await _repository.DbContext.SaveChangesAsync();
+        _unitOfWork.Repository<TE>().Update(entity);
+        await _unitOfWork.SaveChangesAsync();
 
         return id;
     }
@@ -71,12 +68,12 @@ public class BaseService<TE, TF, TV, TA, TU>: IBaseService<TE, TF, TV, TA, TU> w
     public virtual async Task<object> Delete(Guid id)
     {
         /* Validate */
-        var entity = await _repository.GetByIdAsync(id);
+        var entity = await _unitOfWork.Repository<TE>().GetByIdAsync(id);
         if (entity == null)
             throw new NotFoundException();
         /* Save */
-        _repository.Delete(id);
-        await _repository.DbContext.SaveChangesAsync();
+        _unitOfWork.Repository<TE>().Delete(id);
+        await _unitOfWork.SaveChangesAsync();
 
         return id;
     }
@@ -84,7 +81,7 @@ public class BaseService<TE, TF, TV, TA, TU>: IBaseService<TE, TF, TV, TA, TU> w
     public virtual async Task<TV> Get(Guid id)
     {
         /* Validate */
-        var entity = await _repository.GetByIdAsync(id);
+        var entity = await _unitOfWork.Repository<TE>().GetByIdAsync(id);
         if (entity == null)
             throw new NotFoundException();
         
@@ -95,7 +92,7 @@ public class BaseService<TE, TF, TV, TA, TU>: IBaseService<TE, TF, TV, TA, TU> w
     {
         var result = new PagingResponseModel<TV>();
         /* Query */
-        var entities = await _repository.GetAllAsync();
+        var entities = await _unitOfWork.Repository<TE>().GetAllAsync();
         /* Builder */
         var entityModels = _mapper.Map<List<TE>, List<TV>>(entities);
 
